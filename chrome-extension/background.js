65ae0657a89735ab66816d0024523eb31e8d79ee
Yohay Nahmany <yohainjug@gmail.com>
2020-01-20 15:19:02 +0200

    fixing data on body

--- chrome-extension/background.js
+++ chrome-extension/background.js
@@ -1,27 +1,3 @@
-// function createMenu() {
-//   const menus = [
-//     { id: 'devtools-left', title: 'To left' },
-//     { id: 'devtools-right', title: 'To right' },
-//     { id: 'devtools-bottom', title: 'To bottom' },
-//     { id: 'devtools-panel', title: 'Open in a panel (enable in browser settings)' },
-//     { id: 'devtools-remote', title: 'Open Remote DevTools' }
-//   ];
-
-//   let shortcuts = {};
-//   chrome.commands.getAll(commands => {
-//     commands.forEach(({ name, shortcut }) => {
-//       shortcuts[name] = shortcut;
-//     });
-
-//     menus.forEach(({ id, title }) => {
-//       chrome.contextMenus.create({
-//         id: id,
-//         title: title + (shortcuts[id] ? ' (' + shortcuts[id] + ')' : ''),
-//         contexts: ['all']
-//       });
-//     });
-//   });
-// }
 chrome.runtime.onInstalled.addListener(function() {
   chrome.storage.sync.set({color: '#3aa757'}, function() {
     console.log('The color is green.');
@@ -47,13 +23,6 @@ chrome.runtime.onInstalled.addListener(function() {
     contexts: ['all']
   });
   chrome.contextMenus.onClicked.addListener(function(data) {
-    console.log('adding listner', data)
-//     editable: false
-// frameId: 0
-// menuItemId: "saveCodeSnippet"
-// pageUrl: "chrome://extensions/"
-// selectionText: "lflamoameidbdinphdjhmpbkimfobcdc"
-// ?pageUrl=${data.pageUrl}&action=${data.menuItemId}&selectionText=${data.selectionText}
     const url = 'http://localhost:3009/api/v1/snippets';
     fetch(url, {
       method: 'POST', // *GET, POST, PUT, DELETE, etc.
@@ -62,23 +31,15 @@ chrome.runtime.onInstalled.addListener(function() {
       credentials: 'same-origin', // include, *same-origin, omit
       headers: {
         'Content-Type': 'application/json'
-        // 'Content-Type': 'application/x-www-form-urlencoded',
       },
       redirect: 'follow', // manual, *follow, error
       referrerPolicy: 'no-referrer', // no-referrer, *client
       body: JSON.stringify({
         menuItemId: data.menuItemId,
-        selectionText: data.selectionText,
+        saveCodeSnippet: data.selectionText,
         pageUrl: data.pageUrl
       }) // body data type must match "Content-Type" header
     }).then(d => console.log('response from server',d))
     .catch(err => console.error(err));
-    // fetch(`http://localhost:3009/api/v1/save`, {
-    //   method: 'POST',
-    //   body: JSON.stringify(data)
-    // })
   });
 });
-// chrome.runtime.onInstalled.addListener(() => {
-//   createMenu();
-// });
