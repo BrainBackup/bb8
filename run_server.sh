16ddc8774d9f5c300ad537518aed6ae93a613518
Yohay Nahmany <yohainjug@gmail.com>
2020-01-23 08:32:36 +0200

    working on dockerizing

--- run_server.sh
+++ run_server.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+dir=$(pwd)
 cd server
 docker build -t code_snipper_server_api .
-docker run -p 9200:9200 -t code_snipper_server_api 
+docker run -p 9200:9200 -v $dir:/app -t code_snipper_server_api 
