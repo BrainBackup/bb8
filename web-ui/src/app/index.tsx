ea85527470599fdf358713f2e6d70e6ac38cde02
Yohay Nahmany <yohainjug@gmail.com>
2020-02-07 09:34:57 +0200

    working on private routing

--- web-ui/src/app/index.tsx
+++ web-ui/src/app/index.tsx
@@ -3,13 +3,17 @@ import { hot } from 'react-hot-loader/root';
 import { Router, Route, Switch } from 'react-router';
 import { Root } from 'app/containers/Root';
 import { TodoApp } from 'app/containers/TodoApp';
+import PrivateRoute from 'app/components/PrivateRoute';
+import Login from 'app/routes/authentication';
 
 // render react DOM
 export const App = hot(({ history }) => (
   <Root>
     <Router history={history}>
       <Switch>
-        <Route path="/" component={TodoApp} />
+        <Route path="/login" component={Login} />
+        <PrivateRoute address='/' component={TodoApp} authed={false} key={'page.url'} exact/>
+        {/* <Route path="/" component={TodoApp} /> */}
       </Switch>
     </Router>
   </Root>
