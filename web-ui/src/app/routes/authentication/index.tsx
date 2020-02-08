66893c7ae4d69b5b238bafb0e1c3999d6b5978d5
Yohay Nahmany <yohainjug@gmail.com>
2020-02-08 11:52:31 +0200

    centering the card

--- web-ui/src/app/routes/authentication/index.tsx
+++ web-ui/src/app/routes/authentication/index.tsx
@@ -9,16 +9,21 @@
 import * as React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
+// import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
+// import Button from '@material-ui/core/Button';
+// import Typography from '@material-ui/core/Typography';
 // import Username from './components/Username';
+// import { Divider } from '@material-ui/core';
+import { Grid } from '@material-ui/core';
 import CustomInput from 'app/components/CustomInput';
 
 const useStyles = makeStyles({
   root: {
-    minWidth: 275,
+    position: 'fixed',
+    left: '30%',
+    top: '50%',
+    minWidth: 600,
   },
   bullet: {
     display: 'inline-block',
@@ -35,30 +40,21 @@ const useStyles = makeStyles({
 
 export default function OutlinedCard() {
   const classes = useStyles();
-  const bull = <span className={classes.bullet}>•</span>;
-
+  // const bull = <span className={classes.bullet}>•</span>;
   return (
     <Card className={classes.root} variant="outlined">
       <CardContent>
-        <CustomInput label='Username' onChange={(e) => console.log(e.target.value)} />
-        <Typography className={classes.title} color="textSecondary" gutterBottom>
-          Word of the Day
-        </Typography>
-        <Typography variant="h5" component="h2">
-          be{bull}nev{bull}o{bull}lent
-        </Typography>
-        <Typography className={classes.pos} color="textSecondary">
-          adjective
-        </Typography>
-        <Typography variant="body2" component="p">
-          well meaning and kindly.
-          <br />
-          {'"a benevolent smile"'}
-        </Typography>
+        <Grid container>
+          <Grid item xs={6}>
+          </Grid>
+          <Grid item xs={6}>
+            <CustomInput label='Username' onChange={(e) => console.log(e.target.value)} />
+          </Grid>
+        </Grid>
       </CardContent>
-      <CardActions>
+      {/* <CardActions>
         <Button size="small">Learn More</Button>
-      </CardActions>
+      </CardActions> */}
     </Card>
   );
 }
