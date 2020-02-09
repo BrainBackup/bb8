e4c31ae59e443c5d49c61d0cd48a71e502480e53
Yohay Nahmany <yohainjug@gmail.com>
2020-02-09 22:51:03 +0200

    adding login

--- web-ui/src/app/routes/authentication/components/Login.tsx
+++ web-ui/src/app/routes/authentication/components/Login.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { Button } from '@material-ui/core';
 import CustomInput from 'app/components/CustomInput';
 import Password from 'app/components/Password';
 
@@ -8,8 +9,15 @@ interface ILogin {
 const Login: React.FunctionComponent<ILogin> = ({ }) => {
     return (
         <div>
+            {/* TODO: add header */}
             <CustomInput label='Username' onChange={(e) => console.log(e.target.value)} />
             <Password onChange={(e) => console.log(e.target.value)}/>
+            <Button
+                variant="contained"
+                style={{ backgroundColor: '#182952', color: 'white', textTransform: 'none', margin: '5%' }}
+                fullWidth>
+                Login
+            </Button>
         </div>
     )
 };
