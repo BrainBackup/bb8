a6d7b46b9f5e7112ee6dcf2f8408d3be1c266f60
Yohay Nahmany <yohainjug@gmail.com>
2020-02-10 08:42:57 +0200

    adding hook for login

--- web-ui/src/app/routes/authentication/components/Login.tsx
+++ web-ui/src/app/routes/authentication/components/Login.tsx
@@ -1,16 +1,25 @@
 import * as React from 'react';
-import { Button, Typography } from '@material-ui/core';
+import { Button } from '@material-ui/core';
 import CustomInput from 'app/components/CustomInput';
 import Password from 'app/components/Password';
 
 interface ILogin {
 
 }
+interface IForm {
+    username: string,
+    password: string
+};
 const Login: React.FunctionComponent<ILogin> = ({ }) => {
+    const initialState: IForm = {
+        username: '',
+        password: ''
+    };
+    const [form, setForm] = React.useState(initialState);
     return (
         <div>
-            <CustomInput fullWidth={true} label='Username' onChange={(e) => console.log(e.target.value)} />
-            <Password onChange={(e) => console.log(e.target.value)}/>
+            <CustomInput fullWidth={true} label='Username' onChange={(e) => setForm({ ...form, username: e.target.value })} />
+            <Password onChange={(e) => setForm({ ...form, password: e.target.value })}/>
             <Button
                 variant="contained"
                 style={{ backgroundColor: '#182952', color: 'white', textTransform: 'none', margin: '5%' }}
