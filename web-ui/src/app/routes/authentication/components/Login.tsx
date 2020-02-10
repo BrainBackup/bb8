b63fed9500f541b0ac5c4312385f27af1e2da0ba
Yohay Nahmany <yohainjug@gmail.com>
2020-02-10 08:40:11 +0200

    login and register are in place

--- web-ui/src/app/routes/authentication/components/Login.tsx
+++ web-ui/src/app/routes/authentication/components/Login.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Button } from '@material-ui/core';
+import { Button, Typography } from '@material-ui/core';
 import CustomInput from 'app/components/CustomInput';
 import Password from 'app/components/Password';
 
@@ -9,8 +9,7 @@ interface ILogin {
 const Login: React.FunctionComponent<ILogin> = ({ }) => {
     return (
         <div>
-            {/* TODO: add header */}
-            <CustomInput label='Username' onChange={(e) => console.log(e.target.value)} />
+            <CustomInput fullWidth={true} label='Username' onChange={(e) => console.log(e.target.value)} />
             <Password onChange={(e) => console.log(e.target.value)}/>
             <Button
                 variant="contained"
