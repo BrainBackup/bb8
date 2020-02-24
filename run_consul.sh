b7252e820ec89635b4f44d6b70835d8a8bd6134b
Yohay Nahmany <yohainjug@gmail.com>
2020-02-24 11:37:12 +0200

    removing consul

--- run_consul.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-docker run \
-    -d \
-    -p 8500:8500 \
-    -p 8600:8600/udp \
-    --name=consul_new \
-    consul agent -server -ui -node=server-1 -bootstrap-expect=1 -client=0.0.0.0
