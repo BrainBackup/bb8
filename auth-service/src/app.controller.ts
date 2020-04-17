250b1d1fc942dc0c4f34a882def7081119fb6a6f
Yohay Nahmany <yohainjug@gmail.com>
2020-04-18 00:12:23 +0300

    checking the github actions

--- auth-service/src/app.controller.ts
+++ auth-service/src/app.controller.ts
@@ -7,7 +7,7 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get('/health')
-  checkHealth(): HealthCheck {
+  checkHealth() {
     return this.appService.checkHealth();
   }
 }
