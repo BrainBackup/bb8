4fd155c84c194c36e005b876afbd5b8131554428
Yohay Nahmany <yohainjug@gmail.com>
2020-05-11 22:51:26 +0300

    trying to add tests.

--- auth-service/src/auth/auth.controller.spec.ts
+++ auth-service/src/auth/auth.controller.spec.ts
@@ -1,14 +1,18 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { JwtService } from '@nestjs/jwt';
+import { UsersModule } from '../users/users.module';
+import { AuthModule } from './auth.module';
 
 describe('AuthController', () => {
   let authController: AuthController;
 
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
+      imports: [AuthModule],
       controllers: [AuthController],
-      providers: [AuthService],
+      providers: [AuthService, JwtService],
     }).compile();
 
     authController = app.get<AuthController>(AuthController);
@@ -16,7 +20,7 @@ describe('AuthController', () => {
 
   describe('root', () => {
     it('Login', () => {
-      expect(authController.login).toBe('ynahmany test');
+      expect(authController).toBeDefined();
     });
   });
 });
